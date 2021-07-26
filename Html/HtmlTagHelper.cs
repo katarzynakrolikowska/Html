4dae8da3d5050b0ef742ca825b0542504167b1c4
Katarzyna Królikowska <katarzyna.anna.krolikowska@gmail.com>
2021-07-26 21:43:58 +0200

    refactoring

---
+++ Html/HtmlTagHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Html
+{
+    public class HtmlTagHelper
+    {
+        public const char OPEN_TAG = '<';
+
+        public const char CLOSE_TAG = '>';
+
+        public const string COMMENT_OPEN = "<!---";
+
+        public const string COMMENT_CLOSE = "-->";
+
+        public const string EXTENSION = ".html";
+
+        public static List<string> GetTagContentList(string content)
+        {
+            var list = new List<string>();
+            StringBuilder sb = new StringBuilder();
+
+            var contentToCheck = content.Trim().Replace(COMMENT_OPEN, "").Replace(COMMENT_CLOSE, "");
+
+            if (!contentToCheck.Any(c => c == OPEN_TAG) && !contentToCheck.Any(c => c == CLOSE_TAG))
+            {
+                list.Add(contentToCheck);
+            }
+            else if (contentToCheck.Any())
+            {
+                if (contentToCheck.First() != OPEN_TAG)
+                {
+                    var text = GetString(contentToCheck.TakeWhile(c => c != OPEN_TAG));
+                    list.Add(text);
+
+                    sb.Append(GetString(contentToCheck.SkipWhile(c => c != OPEN_TAG)));
+                }
+                else
+                {
+                    sb.Append(GetString(contentToCheck.SkipWhile(c => c != CLOSE_TAG).Skip(1)));
+                }
+
+                list = list.Concat(GetTagContentList(sb.ToString())).ToList();
+            }
+
+            return list;
+        }
+
+        private static string GetString(IEnumerable<char> charList)
+        {
+            return new string(charList.ToArray());
+        }
+    }
+}
--- Html/MainWindow.xaml.cs
+++ Html/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +21,6 @@ namespace Html
     /// </summary>
     public partial class MainWindow : Window
     {
-        public const char OPEN_TAG = '<';
-
-        public const char CLOSE_TAG = '>';
-
-        public const string COMMENT_OPEN = "<!---";
-
-        public const string COMMENT_CLOSE = "-->";
-
         public const string EXTENSION = ".html";
 
         public MainWindow()
@@ -37,8 +30,7 @@ namespace Html
 
         private void saveFileButton_Click(object sender, RoutedEventArgs e)
         {
-            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
 
             openFileDialog.DefaultExt = ".html";
             openFileDialog.Filter = "Html documents (.html)|*.html";
@@ -48,12 +40,12 @@ namespace Html
             if (result == true)
             {
                 string extension = System.IO.Path.GetExtension(openFileDialog.FileName);
+
                 if (extension == EXTENSION)
                 {
                     var content = System.IO.File.ReadAllText(openFileDialog.FileName);
-                    var list = new List<string>();
 
-                    SetTextContent(content, list);
+                    var list = HtmlTagHelper.GetTagContentList(content);
 
                     itemsListView.ItemsSource = list;
                 }
@@ -61,39 +53,7 @@ namespace Html
                 {
                     MessageBox.Show("Podaj plik HTML", "Uwaga", MessageBoxButton.OK);
                 }
-                
             }
-        }
-
-        private void SetTextContent(string content, List<string> list)
-        {
-            var contentToCheck = content.Trim().Replace(COMMENT_OPEN, "").Replace(COMMENT_CLOSE, "");
-
-            if (!contentToCheck.Any(c => c == OPEN_TAG) && !contentToCheck.Any(c => c == CLOSE_TAG))
-            {
-                list.Add(contentToCheck);
-            }
-            else if (contentToCheck.Any())
-            {
-                if (contentToCheck.First() != OPEN_TAG)
-                {
-                    var text = GetString(content.TakeWhile(c => c != OPEN_TAG));
-                    list.Add(text);
-
-                    var newContent = GetString(content.SkipWhile(c => c != OPEN_TAG));
-                    SetTextContent(newContent, list);
-                }
-                else
-                {
-                    var newContent = GetString(contentToCheck.SkipWhile(c => c != CLOSE_TAG).Skip(1));
-                    SetTextContent(newContent, list);
-                }
-            }
-        }
-
-        private string GetString(IEnumerable<char> charList)
-        {
-            return new string(charList.ToArray());
         }
     }
 }
